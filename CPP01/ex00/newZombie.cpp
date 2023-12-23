b1646ce0b0c4d3e355ab64fd5ea188029ffd3dd7
Parallels <francois.gnz@gmail.com>
2023-12-23 23:13:48 +0100

    headers

--- CPP01/ex00/newZombie.cpp
+++ CPP01/ex00/newZombie.cpp
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgonzale <fgonzale@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/23 23:12:46 by fgonzale          #+#    #+#             */
+/*   Updated: 2023/12/23 23:12:48 by fgonzale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Zombie.hpp"
-
-
 
 Zombie	*newZombie(std::string name)
 {
 	Zombie	*new_Zombie = new Zombie(name);
+	if (!new_Zombie)
+		return (NULL);
 	return (new_Zombie);
 }
