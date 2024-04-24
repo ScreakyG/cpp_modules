50ab6d5e36825dd08466113bc5501dd4bb586682
Parallels <francois.gnz@gmail.com>
2024-04-24 21:20:01 +0200

    added tests

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -1,51 +1,61 @@
 #include "Bureaucrat.hpp"
 
+void	gradeTests(const std::string &name, int grade)
+{
+	try
+	{
+		Bureaucrat test(name, grade);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << RED << e.what() << RESET << std::endl;
+	}
+}
+
 int main(void)
 {
-    try
-    {
-        Bureaucrat  og("Og", 1);
-		Bureaucrat	copy = og;
+	// Bureaucrat creation tests , can't initiate if grade has wrong value
 
-    }
-    catch (const std::exception &e)
-    {
+	gradeTests("Joe", 1);
+	gradeTests("Jack", 0);
+	gradeTests("Levi", 100);
+	gradeTests("Dummy", 151);
+
+	// Copy & Assignement tests
+
+	try
+	{
+		Bureaucrat og("Og", 10);
+		std::cout << og << std::endl;
+
+		Bureaucrat	copy(og);
+		std::cout << copy << std::endl;
+
+		Bureaucrat	bill("Bill", 100);
+		Bureaucrat	jack("Jack", 50);
+		jack = bill;
+
+		std::cout << jack;
+	}
+	catch (const std::exception &e)
+	{
 		std::cerr << RED << e.what() << RESET << std::endl;
-    }
-    // try
-    // {
-    //     Bureaucrat test("Joe", 10000000);
-    // }
-    // catch (std::exception &e)
-    // {
-    //     std::cerr << RED << e.what() << RESET << std::endl;
-    // }
-    // try
-    // {
-    //     Bureaucrat jack("Jack", 0);
-    // }
-    // catch (std::exception &e)
-    // {
-            //std::cerr << RED << e.what() << RESET << std::endl;
-    // }
-    // try
-    // {
-    //     Bureaucrat bill("Bill", 1);
-    //     bill.increase_grade();
-    //     std::cout << bill.getGrade() << std::endl;
-    // }
-    // catch (std::exception const &e)
-    // {
-    //     std::cerr << RED << e.what() << RESET << std::endl;
-    // }
-    // try
-    // {
-    //     Bureaucrat cahiman("Cahiman", 149);
-    //     cahiman.decrease_grade();
-    //     std::cout << cahiman;
-    // }
-    // catch (std::exception const &e)
-    // {
-    //     std::cerr << RED << e.what() << RESET << std::endl;
-    // }
+	}
+
+	// Increase / Decrease tests
+
+	try
+	{
+		Bureaucrat ceo("Logan", 1);
+		ceo.decrease_grade();
+		std::cout << ceo;
+
+		Bureaucrat kendall("Kendall", 150);
+		kendall.increase_grade();
+		std::cout << kendall;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << RED << e.what() << RESET << std::endl;
+	}
 }
