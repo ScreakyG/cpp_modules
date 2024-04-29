876a804e15319bcaa45e245a14c96ce7a10b1e53
fgonzale <francois.gnz@gmail.com>
2024-04-29 22:18:56 +0200

    tests

--- CPP05/ex01/main.cpp
+++ CPP05/ex01/main.cpp
@@ -1,21 +1,51 @@
 #include "Bureaucrat.hpp"
 #include "Form.hpp"
 
-
-
-int main(void)
+void	createForms(std::string name, int signGrade, int execGrade)
 {
 	try
 	{
-		Form test("Contrat", 1, 150);
-		Bureaucrat ceo("Logan", 1);
-		Bureaucrat dop("Roman", 2);
-		test.beSigned(ceo);
-		test.beSigned(dop);
-		ceo.signForm(test);
-		dop.signForm(test);
+		std::cout << CYAN << "Creating form named " << name << RESET << std::endl;
+		Form	form(name, signGrade, execGrade);
 	}
-	catch (const std::exception &e)
+	catch(std::exception &e)
+	{
+		std::cerr << RED << e.what() << RESET << std::endl;
+	}
+}
+
+int main(void)
+{
+	// Creating Forms Tests
+
+	createForms("Stage", 1, 1);
+	createForms("Wrong", 1, 151);
+	createForms("Wrong2", 0, 150);
+
+	std::cout << "<---------------------->" << std::endl;
+	try
+	{
+	// Copy Tests
+
+	Form original("Contrat", 5, 25);
+	std::cout << original;
+	Form copy(original);
+	std::cout << copy;
+
+	std::cout << "<---------------------->" << std::endl;
+
+	Form test = Form("Test1", 100, 100);
+	std::cout << test;
+
+	std::cout << "<---------------------->" << std::endl;
+
+	// Sign form tests
+
+	Bureaucrat	ceo("Logan", 100);
+	original.beSigned(ceo);
+	ceo.signForm(original);
+	}
+	catch (std::exception &e)
 	{
 		std::cerr << RED << e.what() << RESET << std::endl;
 	}
