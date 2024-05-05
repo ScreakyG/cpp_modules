fcd99a681d2f1eb6161cf94d46a42878c71d4e14
fgonzale <francois.gnz@gmail.com>
2024-05-05 03:51:56 +0200

    changed test

--- CPP06/ex01/Serializer.hpp
+++ CPP06/ex01/Serializer.hpp
@@ -4,6 +4,14 @@
 #include <iostream>
 #include <stdint.h>
 
+#define RESET	"\e[0m"
+#define RED		"\e[31m"
+#define GREEN	"\e[32m"
+#define YELLOW	"\e[33m"
+#define BLUE	"\e[34m"
+#define PURPLE	"\e[35m"
+#define CYAN	"\e[36m"
+
 class Data;
 
 class Serializer
--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -3,20 +3,25 @@
 
 int main(void)
 {
-//	uintptr_t adress;
+	uintptr_t serialized;
 	Data test1 = {10};
 
 	std::cout << "test1 value : " << test1.value << std::endl;
 	std::cout << "Adress of object test1 : " << &test1 << std::endl;
 
-	std::cout << "uintptr_t value of test1 adress : " << Serializer::serialize(&test1) << std::endl;
-	std::cout << "Adress of object test1 after deserializer : " << Serializer::deserialize(Serializer::serialize(&test1)) << std::endl;
+	std::cout << std::endl;
 
-	// std::cout << "Original pointer adress = " << test << std::endl;
-	// adress = Serializer::serialize(test);
+	serialized = Serializer::serialize(&test1);
+	std::cout << "uintptr_t from serialized test1 adress : " << serialized << std::endl;
 
-	// std::cout << adress << std::endl;
-	// std::cout << Serializer::deserialize(adress) << std::endl;
-	// if (test)
-	// 	delete test;
+	Data *deserialized = Serializer::deserialize(serialized);
+	std::cout << "Adress pointed to after deserialize : " << deserialized << std::endl;
+
+	if (deserialized == &test1)
+	{
+		std::cout << GREEN << "New pointer points to the adress of test1" << RESET << std::endl;
+		std::cout << "Value of deserialized uintptr_t object : " << deserialized->value << std::endl;
+	}
+	else
+		std::cout << RED << "New pointer does not points to the adress of test1" << RESET << std::endl;
 }
