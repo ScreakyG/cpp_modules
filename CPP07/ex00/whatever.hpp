171559bd3a9d9eaa7fcdf7714cd9c3c3c9e7a931
fgonzale <francois.gnz@gmail.com>
2024-05-08 18:56:16 +0200

    ex00 push

---
+++ CPP07/ex00/whatever.hpp
@@ -0,0 +1,25 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template< typename T >
+const T&	max(const T &x, const T &y)
+{
+	return (x > y ? x : y);
+}
+
+template< typename T >
+const T&	min(const T &x, const T&y)
+{
+	return (x < y ? x : y);
+}
+
+template <typename T >
+void	swap(T &x, T &y)
+{
+	T temp;
+	temp = x;
+	x = y;
+	y = temp;
+}
+
+#endif
