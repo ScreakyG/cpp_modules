885273e10f82d61f9b902f243c360f461e1e6d8e
François GONZALEZ <francois.gnz@gmail.com>
2024-07-02 22:45:42 +0200

    progess

--- CPP09/ex01/RPN.cpp
+++ CPP09/ex01/RPN.cpp
@@ -11,8 +11,116 @@ RPN& RPN::operator=(const RPN &rhs)
 	return (*this);
 }
 
+static void	performOperation(std::string &value, std::stack<double> &stack)
+{
+	double	a;
+	double	b;
+	double	result;
+
+	if (stack.size() < 2)
+		throw std::runtime_error("invalid expression");
+	if (value == "+")
+	{
+		a = stack.top();
+		stack.pop();
+		b = stack.top();
+		stack.pop();
+
+		result = b + a;
+		stack.push(result);
+	}
+	else if (value == "-")
+	{
+		a = stack.top();
+		stack.pop();
+		b = stack.top();
+		stack.pop();
+
+		result = b - a;
+		stack.push(result);
+	}
+		else if (value == "*")
+	{
+		a = stack.top();
+		stack.pop();
+		b = stack.top();
+		stack.pop();
+
+		result = b * a;
+		stack.push(result);
+	}
+		else if (value == "/")
+	{
+		a = stack.top();
+		stack.pop();
+		b = stack.top();
+		stack.pop();
+
+		result = b / a;
+		stack.push(result);
+	}
+}
+
+static bool isValidOperand(std::string &value)
+{
+	double	operand;
+	char	*endptr = NULL;
+
+	if (value.empty())
+		return (false);
+	operand = std::strtod(value.c_str(), &endptr);
+	if (*endptr != '\0')
+		throw std::runtime_error("invalid expression");
+	if (operand < 0 || operand > 10)
+		throw std::runtime_error("invalid expression");
+	return (true);
+}
+
+static bool isOperator(std::string &value)
+{
+	if (value == "+" || value == "-" || value == "*" || value == "/")
+		return (true);
+	return (false);
+}
+
 void RPN::Calculate(char *argv)
 {
-	(void)argv;
-	std::cout << "Calling Calculte function" << std::endl;
+
+	std::string				expression(argv);
+	std::size_t				found = 0;
+	std::string				value;
+	std::stack<double>			stack;
+
+	try
+	{
+		found = expression.find(" ");
+		while (found != std::string::npos)
+		{
+			value = expression.substr(0, found);
+			expression = expression.substr(found + 1);
+			if (isOperator(value) == true)
+				performOperation(value, stack);
+			else if (isValidOperand(value) == true)
+				stack.push(std::strtod(value.c_str(), NULL));
+			found = expression.find(" ");
+
+			if (found == std::string::npos)
+			{
+				value = expression.substr(0, found);
+				if (value.empty())
+					break ;
+				else if (isOperator(value) == true)
+					performOperation(value, stack);
+				else
+					throw std::runtime_error("invalid expression");
+			}
+		}
+		if (stack.size() != 1)
+			throw std::runtime_error("invalid expression");
+		std::cout << stack.top() << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Error : " << e.what() << std::endl;
+	}
 }
