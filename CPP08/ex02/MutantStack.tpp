02240ba1104a96d0e095152a76cbe39bcc83f52a
Parallels <francois.gnz@gmail.com>
2025-03-04 11:52:52 +0100

    fixed all iterators

--- CPP08/ex02/MutantStack.tpp
+++ CPP08/ex02/MutantStack.tpp
@@ -1,26 +1,26 @@
 #include "MutantStack.hpp"
 
-template<typename T>
-MutantStack<T>::MutantStack(void): std::stack<T>()
+template<typename T, typename C>
+MutantStack<T, C>::MutantStack(void): std::stack<T, C>()
 {
 	std::cout << "MutantStack default constructor called" << std::endl;
 }
 
-template<typename T>
-MutantStack<T>::MutantStack(const MutantStack<T> &src): std::stack<T>(src)
+template<typename T, typename C>
+MutantStack<T, C>::MutantStack(const MutantStack<T, C> &src): std::stack<T, C>(src)
 {
 	std::cout << "MutantStack copy constructor called" << std::endl;
 	// *this = src;
 }
 
-template<typename T>
-MutantStack<T>::~MutantStack(void)
+template<typename T, typename C>
+MutantStack<T, C>::~MutantStack(void)
 {
 	std::cout << "MutantStack destructor called" << std::endl;
 }
 
-template<typename T>
-MutantStack<T>& MutantStack<T>::operator=(const MutantStack<T> &rhs)
+template<typename T, typename C>
+MutantStack<T, C>& MutantStack<T, C>::operator=(const MutantStack<T, C> &rhs)
 {
 	if (this != &rhs)
 	{
--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -1,5 +1,6 @@
 #include "MutantStack.hpp"
 #include <list>
+#include <vector>
 
 # define RESET	"\e[0m"
 # define BLACK	"\e[30m"
@@ -101,9 +102,43 @@ static void assignementTests()
     std::cout << std::endl;
 }
 
+static void othersTests(void)
+{
+    std::cout << GREEN "##### OTHERS TESTS #####\n" RESET;
+
+    MutantStack<double, std::vector<double> > mstack;
+    MutantStack<double, std::vector<double> >::const_iterator cit;
+    MutantStack<double, std::vector<double> >::reverse_iterator rit;
+    MutantStack<double, std::vector<double> >::const_reverse_iterator crit;
+
+    mstack.push(42.42);
+    mstack.push(22.22);
+
+    std::cout << "Using const_iterator : " << std::endl;
+    for (cit = mstack.cbegin(); cit != mstack.cend(); cit++)
+        std::cout << *cit << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Using reverse_iterator : " << std::endl;
+    for (rit = mstack.rbegin(); rit != mstack.rend(); rit++)
+        std::cout << *rit << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Using const_reverse_iterator : " << std::endl;
+    for (crit = mstack.crbegin(); crit != mstack.crend(); crit++)
+        std::cout << *crit << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Top element = " << mstack.top() << std::endl;
+    std::cout << "Size = " << mstack.size() << std::endl;
+
+    std::cout << std::endl;
+}
+
 int main()
 {
     subjectTests();
     listCompare();
     assignementTests();
+    othersTests();
 }
